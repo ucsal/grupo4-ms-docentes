a8ede66799b5d97afeee96d5f2becdbbcd2dcdd8
AlannaFM <alannamachado84@gmail.com>
2026-06-03 22:35:22 -0300

    feat: repository e service do professor

---
+++ src/main/java/org/example/docentes/repository/ProfessorRepository.java
@@ -0,0 +1,11 @@
+package org.example.docentes.repository;
+
+
+import org.example.docentes.entity.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    List<Professor> findByStatusAtivo(boolean statusAtivo);
+}
