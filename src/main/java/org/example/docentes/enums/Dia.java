2d4897b84ee8964a8c84372e0aabf5dd8bf2791f
AlannaFM <alannamachado84@gmail.com>
2026-06-03 23:04:37 -0300

    feat: enums dia e turno

---
+++ src/main/java/org/example/docentes/enums/Dia.java
@@ -0,0 +1,10 @@
+package org.example.docentes.enums;
+
+public enum Dia {
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO
+}
---
+++ src/main/java/org/example/docentes/enums/Turno.java
@@ -0,0 +1,7 @@
+package org.example.docentes.enums;
+
+public enum Turno {
+    MANHA,
+    TARDE,
+    NOITE
+}
