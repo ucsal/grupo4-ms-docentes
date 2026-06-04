28876cdf1f6cffd97bfdaa06e85ddf32849bf571
AlannaFM <alannamachado84@gmail.com>
2026-06-03 21:06:39 -0300

    Primeiro commit

---
+++ src/main/java/org/example/docentes/DocentesApplication.java
@@ -0,0 +1,13 @@
+package org.example.docentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocentesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocentesApplication.class, args);
+    }
+
+}
